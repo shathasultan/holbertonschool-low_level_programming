049a8f1dd27b741eed5794505f93d920efaa8a0c
shathasultan <shathasultann9@gmail.com>
2025-08-24 19:06:11 +0000

    Task 4: Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -10,45 +9,3 @@ int main(void)
     puts("\"Programming is like building a multilingual puzzle");
     return (0);
 }
-~
-
-~
-
-~
-
-~
-
-~
-
-~
-
-~
-
-~
-
-~
-
-~
-
-~
-
-~
-
-~
-
-~
-
-~
-
-~
-
-~
-
-~
-
-~
-
-~
-
-~
-"4-puts.c" 12, 168B
