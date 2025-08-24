1efbc77bccd6927ff890c16639b00914084b08b7
shathasultan <shathasultann9@gmail.com>
2025-08-24 19:02:12 +0000

    Task 4: Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -10,4 +10,45 @@ int main(void)
     puts("\"Programming is like building a multilingual puzzle");
     return (0);
 }
+~
 
+~
+
+~
+
+~
+
+~
+
+~
+
+~
+
+~
+
+~
+
+~
+
+~
+
+~
+
+~
+
+~
+
+~
+
+~
+
+~
+
+~
+
+~
+
+~
+
+~
+"4-puts.c" 12, 168B
