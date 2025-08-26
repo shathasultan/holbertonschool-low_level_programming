ff74de77678e535fa13d4d563f029604e5935412
shathasultan <shathasultann9@gmail.com>
2025-08-26 09:04:18 +0000

    Fix Betty style for 6-abs.c

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,14 +1,18 @@
 #include "main.h"
 /**
- * _abs - computes the absolute value of an integer
- * @n: integer to compute
- *
- * Return: absolute value
- */
+* _abs - computes the absolute value of an integer
+* @n: The integer to compute
+*
+* Return: The absolute value of n
+*/
 int _abs(int n)
 {
-if(n<0)
-return(-n);
+if (n < 0)
+{
+return (-n);
+}
 else
-return(n);
+{
+return (n);
+}
 }
