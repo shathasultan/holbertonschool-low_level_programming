69cc35e2bf02885d910aa1fe5360f679967aba89
shathasultan <shathasultann9@gmail.com>
2025-09-04 17:15:51 +0000

    Save local changes before rebase

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,13 @@
-
 #include "main.h"
-
 /**
- * _isupper - checks for uppercase character.
- * @c: int to check (ASCII code of a character)
- *
- * Return: 1 if c is between 'A' and 'Z', 0 otherwise.
- */
+* _isupper - checks for uppercase character
+* @c: the character to check
+*
+* Return: 1 if c is uppercase, 0 otherwise
+*/
 int _isupper(int c)
 {
-  if (c >= 'A' && c <= 'Z')
- return (1);
- return (0);
+if (c >= 'A' && c <= 'Z')
+return (1);
+return (0);
 }
