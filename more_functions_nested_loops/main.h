a802b1ce97c41e29248116e92b5002313a1c5d1c
shathasultan <shathasultann9@gmail.com>
2025-09-04 18:17:52 +0000

    Task 1: _isdigit and Task 2: mul

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,17 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
- * _isupper - checks for uppercase character
- * @c: character to check
- *
- * Return: 1 if c is uppercase, 0 otherwise
- */
 int _isupper(int c);
-/**
- * _isdigit - checks for a digit (0 through 9)
- * @c: character to check
- *
- * Return: 1 if c is a digit, 0 otherwise
- */
 int _isdigit(int c);
+int mul(int a, int b);
 #endif /* MAIN_H */
