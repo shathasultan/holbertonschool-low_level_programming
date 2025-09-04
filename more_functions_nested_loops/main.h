be83ee2dcf25437ca3c72d1af125d534b35493ab
shathasultan <shathasultann9@gmail.com>
2025-09-04 17:55:01 +0000

    Task 1: _isdigit WIP

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,10 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 /**
-* _isupper - checks for uppercase character
-* @c: character to check
-*
-* Return: 1 if c is uppercase, 0 otherwise
-*/
+ * _isupper - checks for uppercase character
+ * @c: character to check
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
 int _isupper(int c);
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: character to check
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c);
 #endif /* MAIN_H */
