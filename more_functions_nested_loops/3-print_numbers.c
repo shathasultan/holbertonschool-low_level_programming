4e5550b3dd86fb598084fc9d5bbb04fbfa7a7d84
shathasultan <shathasultann9@gmail.com>
2025-09-04 19:02:37 +0000

    Update: 3-print_numbers.c and add _putchar.c

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
-* print_numbers - prints the numbers from 0 to 9
+* print_numbers - prints the numbers from 0 to 9, followed by a new line
 *
-* Return: nothing
+* Return: void
 */
 void print_numbers(void)
 {
