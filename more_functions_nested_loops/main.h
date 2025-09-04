6ff214d58e2da35846eb6e4f76863d4e78e71158
shathasultan <shathasultann9@gmail.com>
2025-09-04 17:18:54 +0000

    Update main.h

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,6 +1,10 @@
-
 #ifndef MAIN_H
 #define MAIN_H
-
+/**
+* _isupper - checks for uppercase character
+* @c: character to check
+*
+* Return: 1 if c is uppercase, 0 otherwise
+*/
 int _isupper(int c);
 #endif /* MAIN_H */
