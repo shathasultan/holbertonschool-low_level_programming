74996b3fba23fba54169f153b078690d6303c846
shathasultan <shathasultann9@gmail.com>
2025-09-05 17:53:59 +0000

    Task 10: _atoi with Betty style (handles INT_MIN)

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,2 +1,39 @@
 #include "main.h"
-int _atoi(char *s){int i=0,sign=1,res=0,started=0;while(s[i]){if(s[i]=='-'&&!started)sign=-sign;else if(s[i]=='+'&&!started){}else if(s[i]>='0'&&s[i]<='9'){started=1;break;}i++;}while(s[i]>='0'&&s[i]<='9'){res=res*10-(s[i]-'0');i++;}if(!started)return 0;return sign==1?-res:res;}
+/**
+* _atoi - converts a string to an integer
+* @s: pointer to the string
+*
+* Return: the converted integer, or 0 if no numbers found
+*/
+int _atoi(char *s)
+{
+int i = 0;
+int sign = 1;
+int res = 0;
+int started = 0;
+while (s[i] != '\0')
+{
+if (s[i] == '-' && !started)
+sign = -sign;
+else if (s[i] == '+' && !started)
+{
+/* ignore */
+}
+else if (s[i] >= '0' && s[i] <= '9')
+{
+started = 1;
+break;
+}
+i++;
+}
+while (s[i] >= '0' && s[i] <= '9')
+{
+res = res * 10 - (s[i] - '0');
+i++;
+}
+if (!started)
+return (0);
+if (sign == 1)
+return (-res);
+return (res);
+}
