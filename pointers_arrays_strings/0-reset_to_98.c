4273f77abbb14bc6fe3b03411ec8add5653ef111
shathasultan <shathasultann9@gmail.com>
2025-09-05 16:42:42 +0000

    Task 0: reset_to_98 with clean README and Betty-compliant code

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of an int to 98
+ * @n: pointer to the int to update
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,5 @@
+
+# Pointers, Arrays and Strings
+
+Task 0: Function that takes a pointer to an int and updates the value to 98.
+
