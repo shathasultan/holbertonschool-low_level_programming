dd416cf7653ed3fe0b93f2d9be18050adbf29dbc
shathasultan <shathasultann9@gmail.com>
2025-09-05 17:38:18 +0000

    Task 10: implement _atoi (Betty-compliant)

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+* _atoi - converts a string to an integer
+* @s: pointer to the string
+*
+* Return: the converted integer, or 0 if no numbers found
+*/
+int _atoi(char *s)
+{
+int i = 0;
+int sign = 1;
+unsigned int num = 0;
+int started = 0;
+while (s[i] != '\0')
+{
+if (s[i] == '-' && !started)
+sign = -sign;
+else if (s[i] == '+' && !started)
+{
+/* ignore */
+}
+else if (s[i] >= '0' && s[i] <= '9')
+{
+started = 1;
+break;
+}
+i++;
+}
+while (s[i] >= '0' && s[i] <= '9')
+{
+num = (num * 10) + (s[i] - '0');
+i++;
+}
+return (sign < 0 ? -(int)num : (int)num);
+}
