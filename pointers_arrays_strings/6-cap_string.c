8625dc3018a614014bd13e2db4fded284142d80d
shathasultan <shathasultann9@gmail.com>
2025-09-06 17:42:09 +0000

    Fix: cap_string implementation with Betty style

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,18 +1,20 @@
-نن#include "main.h"
+#include "main.h"
 /**
 * cap_string - capitalizes all words of a string
-* @s: string to modify
+* @s: input string
 *
-* Return: the resulting string
+* Return: pointer to the modified string
 */
 char *cap_string(char *s)
 {
-int i, j;
+int i = 0, j;
 char sep[] = " \t\n,;.!?\"(){}";
-for (i = 0; s[i] != '\0'; i++)
+while (s[i] != '\0')
 {
 if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+{
 s[i] -= 32;
+}
 for (j = 0; sep[j] != '\0'; j++)
 {
 if (s[i] == sep[j] && s[i + 1] >= 'a' && s[i + 1] <= 'z')
@@ -21,6 +23,7 @@ s[i + 1] -= 32;
 break;
 }
 }
+i++;
 }
 return (s);
 }
