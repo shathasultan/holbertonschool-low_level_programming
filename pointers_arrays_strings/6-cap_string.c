5f4524da8c685efa6ba1491270e2f8b2a5af8e5e
shathasultan <shathasultann9@gmail.com>
2025-09-06 17:38:06 +0000

    Task 6: cap_string implementation (Betty-compliant)

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,29 +1,26 @@
-#include "main.h"
+نن#include "main.h"
 /**
 * cap_string - capitalizes all words of a string
 * @s: string to modify
 *
-* Return: pointer to the modified string
+* Return: the resulting string
 */
 char *cap_string(char *s)
 {
-int i = 0, j;
+int i, j;
 char sep[] = " \t\n,;.!?\"(){}";
-while (s[i] != '\0')
+for (i = 0; s[i] != '\0'; i++)
 {
 if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
 s[i] -= 32;
- j = 0;
-while (sep[j] != '\0')
+for (j = 0; sep[j] != '\0'; j++)
 {
 if (s[i] == sep[j] && s[i + 1] >= 'a' && s[i + 1] <= 'z')
 {
 s[i + 1] -= 32;
 break;
 }
-j++;
 }
-i++;
 }
 return (s);
 }
