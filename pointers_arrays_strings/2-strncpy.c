07a85fb46559422fd18ccf2486a449375e847dc3
shathasultan <shathasultann9@gmail.com>
2025-09-06 17:50:02 +0000

    Task 2: fix _strncpy with Betty doc and style

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,18 @@
 #include "main.h"
+/**
+* _strncpy - copies a string
+* @dest: destination buffer
+* @src: source string
+* @n: maximum number of bytes to copy
+*
+* Return: pointer to dest
+*/
 char *_strncpy(char *dest, char *src, int n)
 {
-int i = 0;
-while (i < n && src[i] != '\0')
+int i;
+for (i = 0; i < n && src[i] != '\0'; i++)
 {
 dest[i] = src[i];
-i++;
 }
 while (i < n)
 {
