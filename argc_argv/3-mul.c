b23914fa2424f382376b0a9eb82e5ac8efda5a8e
shathasultan <shathasultann9@gmail.com>
2025-09-12 22:19:32 +0000

    argc_argv: task 3 - multiply args (Betty coding & docs OK)

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - multiplies two numbers and prints the result
+* @argc: argument count
+* @argv: argument vector
+*
+* Return: 0 on success; 1 if incorrect number of arguments
+*/
+int main(int argc, char *argv[])
+{
+int a, b, res;
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+res = a * b;
+printf("%d\n", res);
+return (0);
+}
