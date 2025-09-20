e707d586e4a978bcbbf1c96002de30f63165f0ae
shathasultan <shathasultann9@gmail.com>
2025-09-20 19:25:18 +0000

    structures_typedef: Task 0 - define struct dog (Betty OK)

---
+++ structures_typedef/README.md
@@ -0,0 +1,2 @@
+0x0D. C - Structures, typedef
+Task 0: define struct dog
---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - info about a dog
+ * @name: pointer to name
+ * @age: age in years
+ * @owner: pointer to owner's name
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif /* DOG_H */
