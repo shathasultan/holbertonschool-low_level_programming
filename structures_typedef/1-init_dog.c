943619805a5a2c77fd1616884158103108fd6333
shathasultan <shathasultann9@gmail.com>
2025-09-20 19:31:50 +0000

    structures_typedef: fix NULL include and docs for init_dog (Betty OK)

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,12 +1,15 @@
 
 #include "dog.h"
+#include <stddef.h>
+
+
 
 /**
  * init_dog - initializes a variable of type struct dog
  * @d: pointer to struct dog to initialize
- * @name: pointer to name
- * @age: age in years
- * @owner: pointer to owner's name
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
