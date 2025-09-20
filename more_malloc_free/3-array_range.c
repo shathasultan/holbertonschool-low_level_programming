c316085895b0ecce308cc095d29f2c4e1cfdf429
shathasultan <shathasultann9@gmail.com>
2025-09-20 19:14:39 +0000

    more_malloc_free: fix Betty style for 3-array_range.c

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -1,33 +1,24 @@
-
 #include "main.h"
 #include <stdlib.h>
-#include <limits.h>
-
 /**
  * array_range - creates an array of ints from min to max (inclusive)
  * @min: starting value
  * @max: ending value
  *
- * Return: pointer to newly allocated array ordered from min to max,
- *         or NULL if min > max, on overflow, or on allocation failure
+ * Return: pointer to new array ordered from min to max,
+ *         or NULL if min > max or on allocation failure
  */
 int *array_range(int min, int max)
 {
-
 int *arr;
-unsigned int i;
-unsigned long count;
+int i, size;
 if (min > max)
 return (NULL);
-/* number of elements = max - min + 1 (use wider type to avoid UB) */
-count = (unsigned long)((long)max - (long)min) + 1UL;
-/* overflow check for bytes = count * sizeof(int) */
-if (count > (ULONG_MAX / sizeof(int)))
-return (NULL);
-arr = malloc(count * sizeof(int));
+size = max - min + 1;
+arr = malloc(sizeof(int) * size);
 if (arr == NULL)
 return (NULL);
-for (i = 0; i < count; i++)
-arr[i] = min + (int)i;
+for (i = 0; i < size; i++)
+arr[i] = min + i;
 return (arr);
 }
