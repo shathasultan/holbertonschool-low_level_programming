1b4f824b5ed50515cab074882d82e2e31f916f6b
shathasultan <shathasultann9@gmail.com>
2025-09-20 19:51:33 +0000

    structures_typedef: add typedef dog_t for struct dog (Betty OK)

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,20 +1,21 @@
+
 #ifndef DOG_H
 #define DOG_H
 
 /**
  * struct dog - information about a dog
- * @name: dog's name (string)
+ * @name: dog's name
  * @age: dog's age in years
- * @owner: owner's name (string)
+ * @owner: owner's name
  */
 struct dog
 {
-
 char *name;
 float age;
 char *owner;
 };
-/* Prototypes */
+typedef struct dog dog_t;
+/* prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif /* DOG_H */
