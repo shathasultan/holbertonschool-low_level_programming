6730ba0c881df1647cebeec6e03189f3c852a5d8
shathasultan <shathasultann9@gmail.com>
2025-09-27 18:39:44 +0000

    function_pointers: add print_name (task 0)

---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,13 @@
+
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+
+/**
+ * print_name - prototype for print_name
+ * @name: name string to print
+ * @f: pointer to a function that takes a char * and returns void
+ */
+void print_name(char *name, void (*f)(char *));
+
+#endif /* FUNCTION_POINTERS_H */
