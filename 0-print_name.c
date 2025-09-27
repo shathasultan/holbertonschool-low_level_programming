19031e00ae4c7ba45c39c8934415cd9bc2ba1d34
shathasultan <shathasultann9@gmail.com>
2025-09-27 20:08:21 +0000

    Add function pointers exercises 0 and 1

--- 0-print_name.c
+++ 0-print_name.c
@@ -1,41 +1,38 @@
 #include <stdio.h>
 #include "function_pointers.h"
-
 /**
- * print_name - prints a name using a given function
- * @name: name of the person
- * @f: pointer to a function that prints the name
- */
+* print_name - prints a name using a given function
+* @name: name of the person
+* @f: pointer to a function that prints the name
+*/
 void print_name(char *name, void (*f)(char *))
 {
-    if (name && f)
-        f(name);
+if (name && f)
+f(name);
 }
-
 /**
  * print_name_as_is - prints a name as is
  * @name: name of the person
  */
 void print_name_as_is(char *name)
 {
-    printf("Hello, my name is %s\n", name);
+printf("Hello, my name is %s\n", name);
 }
-
 /**
- * print_name_uppercase - prints a name in uppercase
- * @name: name of the person
- */
+* print_name_uppercase - prints a name in uppercase
+* @name: name of the person
+*/
 void print_name_uppercase(char *name)
 {
-    unsigned int i = 0;
-    printf("Hello, my uppercase name is ");
-    while (name[i])
-    {
-        if (name[i] >= 'a' && name[i] <= 'z')
-            putchar(name[i] - 'a' + 'A');
-        else
-            putchar(name[i]);
-        i++;
-    }
-    putchar('\n');
+unsigned int i = 0;
+printf("Hello, my uppercase name is ");
+while (name[i])
+{
+if (name[i] >= 'a' && name[i] <= 'z')
+putchar(name[i] - 'a' + 'A');
+else
+putchar(name[i]);
+i++;
+}
+putchar('\n');
 }
---
+++ 1-array_iterator.c
@@ -0,0 +1,16 @@
+
+include "function_pointers.h"
+/**
+ * array_iterator - executes a function on each element of an array
+ * @array: array of integers
+ * @size: size of the array
+ * @action: pointer to the function to execute
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+size_t i;
+if (array == NULL || action == NULL)
+ return;
+for (i = 0; i < size; i++)
+action(array[i]);
+}
