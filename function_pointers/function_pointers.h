5fe004079e4008ce4e56b44f549f64bd55240a4b
shathasultan <shathasultann9@gmail.com>
2025-09-27 19:46:34 +0000

    Add 0-print_name project files

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,6 +1,11 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+
+/* prototype of print_name function */
 void print_name(char *name, void (*f)(char *));
+
+/* prototypes of the helper functions */
 void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
+
 #endif /* FUNCTION_POINTERS_H */
