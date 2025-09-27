b30a153f0cbbec81d4d7ed26fcef9919ad1b7231
shathasultan <shathasultann9@gmail.com>
2025-09-27 20:02:51 +0000

    Add 0-print_name project files

--- 0-print_name.c
+++ 0-print_name.c
@@ -1,44 +1,41 @@
 #include <stdio.h>
 #include "function_pointers.h"
+
 /**
-* print_name_as_is - prints a name as is
-* @name: name of the person
-*
-* Return: Nothing.
-*/
-void print_name_as_is(char *name)
-{
-printf("Hello, my name is %s\n", name);
-}
-/**
-* print_name_uppercase - prints a name in uppercase
-* @name: name of the person
-*
-* Return: Nothing.
-*/
-void print_name_uppercase(char *name)
-{
-unsigned int i = 0;
-printf("Hello, my uppercase name is ");
-while (name[i])
-{
-if (name[i] >= 'a' && name[i] <= 'z')
-putchar(name[i] - 'a' + 'A');
-else
-putchar(name[i]);
-i++;
-}
-putchar('\n');
-}
-/**
- * print_name - executes a function on a given name
+ * print_name - prints a name using a given function
  * @name: name of the person
- * @f: pointer to function that takes a string and returns void
- *
- * Return: Nothing.
+ * @f: pointer to a function that prints the name
  */
 void print_name(char *name, void (*f)(char *))
 {
-if (name != NULL && f != NULL)
-f(name);
+    if (name && f)
+        f(name);
+}
+
+/**
+ * print_name_as_is - prints a name as is
+ * @name: name of the person
+ */
+void print_name_as_is(char *name)
+{
+    printf("Hello, my name is %s\n", name);
+}
+
+/**
+ * print_name_uppercase - prints a name in uppercase
+ * @name: name of the person
+ */
+void print_name_uppercase(char *name)
+{
+    unsigned int i = 0;
+    printf("Hello, my uppercase name is ");
+    while (name[i])
+    {
+        if (name[i] >= 'a' && name[i] <= 'z')
+            putchar(name[i] - 'a' + 'A');
+        else
+            putchar(name[i]);
+        i++;
+    }
+    putchar('\n');
 }
