21c0142d18c02270dd13923bfb407b1e5d5a65a9
shathasultan <shathasultann9@gmail.com>
2025-09-28 02:49:41 +0000

    variadic_functions: Task 0 - implement sum_them_all (Betty OK)

---
+++ variadic_functions/README.md
@@ -0,0 +1,5 @@
+
+Task 0: sum_them_all
+
+Implements sum_them_all(const unsigned int n, ...) that returns the sum of all its integer arguments.
+
---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,7 @@
+
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+/* prototype for the task */
+int sum_them_all(const unsigned int n, ...);
+#endif /* VARIADIC_FUNCTIONS_H */
