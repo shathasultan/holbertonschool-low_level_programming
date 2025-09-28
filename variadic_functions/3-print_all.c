33f4315e9f290417d6cf1cf7013c3b024b54b420
shathasultan <shathasultann9@gmail.com>
2025-09-28 03:48:15 +0000

    Task 3 - implement print_all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,17 +1,17 @@
 
-#include "variadic_functions.h"
 #include <stdio.h>
-
+#include <stdarg.h>
+#include <stddef.h>
+#include "variadic_functions.h"
 /**
- * print_all - prints anything based on a format string
- * @format: string of types: c (char), i (int), f (float), s (string)
- *
- * Only allowed:
- * - 2 if statements max
- * - 2 while loops max
- * - No else / else if
- * - Max 9 variables
- */
+* print_all - prints anything based on format string
+* @format: string with format specifiers: c (char), i (int),
+* f (float), s (string). Any other char is ignored.
+*
+* Description: If a string argument is NULL, prints (nil) instead.
+* Each printed value is separated by ", " except the last one.
+* Prints a new line at the end.
+*/
 void print_all(const char * const format, ...)
 {
 va_list ap;
@@ -20,9 +20,6 @@ char *s;
 char *sep = "";
 va_start(ap, format);
 while (format && format[i])
-{
-if (format[i] == 'c' || format[i] == 'i' ||
-format[i] == 'f' || format[i] == 's')
 {
 if (format[i] == 'c')
 printf("%s%c", sep, va_arg(ap, int));
@@ -34,12 +31,10 @@ if (format[i] == 's')
 {
 s = va_arg(ap, char *);
 if (!s)
-printf("%s(nil)", sep);
-else
+s = "(nil)";
 printf("%s%s", sep, s);
 }
 sep = ", ";
-}
 i++;
 }
 va_end(ap);
--- variadic_functions/README.md
+++ variadic_functions/README.md
@@ -1,10 +1,6 @@
 
 Task 3: print_all
 Implements print_all(const char * const format, ...).
-Prints values based on format characters:
-c: char
-i: integer
-f: float
-s: string (prints (nil) for NULL)
-No else statements, max 2 ifs, max 2 while loops, max 9 variables.
+Prints anything based on format string: c, i, f, s.
+Handles NULL strings as "(nil)" and separates values with ", ".
 
