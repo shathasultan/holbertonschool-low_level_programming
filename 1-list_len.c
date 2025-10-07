73c093e2b08d36009c702d7d607ac87fef051226
shathasultan <shathasultann9@gmail.com>
2025-10-07 20:21:44 +0000

    Add list_len function and test main

--- 1-list_len.c
+++ 1-list_len.c
@@ -1,17 +1,20 @@
 #include "lists.h"
+
 /**
- * list_len - Returns the number of elements in a linked list_t list
-* @h: Pointer to the first node
-*
-* Return: Number of nodes
-*/
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: pointer to the head of the list
+ *
+ * Return: number of elements
+ */
 size_t list_len(const list_t *h)
 {
-size_t count = 0;
-while (h)
-{
-count++;
-h = h->next;
-}
-return (count);
+    size_t count = 0;
+
+    while (h)
+    {
+        count++;
+        h = h->next;
+    }
+
+    return (count);
 }
