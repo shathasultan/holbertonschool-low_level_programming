8227d2242506b4d7e3d8dde8c74e804e24d1d2ac
shathasultan <shathasultann9@gmail.com>
2025-10-07 20:00:54 +0000

    Fix Betty return statement and comments

--- 1-list_len.c
+++ 1-list_len.c
@@ -1,4 +1,10 @@
 #include "lists.h"
+/**
+ * list_len - Returns the number of elements in a linked list_t list
+* @h: Pointer to the first node
+*
+* Return: Number of nodes
+*/
 size_t list_len(const list_t *h)
 {
 size_t count = 0;
@@ -7,5 +13,5 @@ while (h)
 count++;
 h = h->next;
 }
-return count;
+return (count);
 }
