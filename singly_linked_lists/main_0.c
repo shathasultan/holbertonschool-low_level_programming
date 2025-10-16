75de6c3655f6124f13d8c84defc1f4764ea780e2
shathasultan <shathasultann9@gmail.com>
2025-10-16 19:57:19 +0000

    Fixed comments and adjustments in singly_linked_lists

--- singly_linked_lists/main_0.c
+++ singly_linked_lists/main_0.c
@@ -1,30 +1,24 @@
 #include "lists.h"
 #include <stdlib.h>
 #include <stdio.h>
-
 int main(void)
 {
-    list_t *head;
-    list_t *new;
-    size_t n;
-
-    head = NULL;
-
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    new->str = "Hello";
-    new->len = 5;
-    new->next = head;
-    head = new;
-
-    n = list_len(head);
-    printf("-> %lu elements\n", n);
-
-    free(new);
-
-    return 0;
+list_t *head;
+list_t *new;
+size_t n;
+head = NULL;
+new = malloc(sizeof(list_t));
+if (new == NULL)
+{
+printf("Error\n");
+return (1);
+}
+new->str = "Hello";
+new->len = 5;
+new->next = head;
+head = new;
+n = list_len(head);
+printf("-> %lu elements\n", n);
+free(new);
+return 0;
 }
