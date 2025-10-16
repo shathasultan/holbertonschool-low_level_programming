4beee708e5b88aedf81a7542b48c92404d5c5a22
shathasultan <shathasultann9@gmail.com>
2025-10-16 20:15:27 +0000

    Added parentheses around return statement

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,11 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
+
+/**
+ * list_len - Returns the number of elements in a linked list_t list.
+ * @h: The head of the list.
+ *
+ * Return: The number of elements in the list.
+ */
 size_t list_len(const list_t *h)
 {
-size_t count = 0;
-while (h)
-{
-count++;
-h = h->next;
-}
-return count;
+    const list_t *current = h;
+    size_t count = 0;
+
+    while (current != NULL)
+    {
+        count++;
+        current = current->next;
+    }
+
+    return (count);  // تأكد من إضافة الأقواس هنا حول جملة return
 }
