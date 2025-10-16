3c409d43baf82346f5ca4eb1e1173341e463d6d4
shathasultan <shathasultann9@gmail.com>
2025-10-16 20:06:03 +0000

    Added parentheses around return statement to meet Betty style

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,11 +1,21 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 #include <stddef.h>  /* for NULL and size_t */
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ */
 typedef struct list_s
 {
-char *str;
-unsigned int len;
-struct list_s *next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
+
 size_t list_len(const list_t *h);
-#endif /* LISTS_H */
+
+#endif
