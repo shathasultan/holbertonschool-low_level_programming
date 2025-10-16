a530541695abc945f759027a2f89e495713c069d
shathasultan <shathasultann9@gmail.com>
2025-10-16 20:21:56 +0000

    Added parentheses around return statement

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,12 +1,11 @@
 #include "lists.h"
-
 size_t list_len(const list_t *h)
 {
-    size_t count = 0;
-    while (h)
-    {
-        count++;
-        h = h->next;
-    }
-    return count;
+size_t count = 0;
+while (h)
+{
+count++;
+h = h->next;
+}
+return count;
 }
