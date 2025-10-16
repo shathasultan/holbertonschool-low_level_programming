77d1463081b501867514c6f2ac88cd9bd870296e
shathasultan <shathasultann9@gmail.com>
2025-10-16 20:20:04 +0000

    Modified lists.h

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,15 +1,11 @@
 #ifndef LISTS_H
 #define LISTS_H
-
 #include <stddef.h>  /* for NULL and size_t */
-
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
-
 size_t list_len(const list_t *h);
-
 #endif /* LISTS_H */
