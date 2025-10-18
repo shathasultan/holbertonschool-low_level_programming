c15a503493dbd40a604ee898c0c08f1e04a6bb14
shathasultan <shathasultann9@gmail.com>
2025-10-18 13:58:57 +0000

    Fix modified files and add untracked files

--- singly_linked_lists/1-main.c
+++ singly_linked_lists/1-main.c
@@ -1,32 +1,10 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include "lists.h"
-/**
-* main - check the code
-*
-* Return: Always 0.
-*/
+
+#include <stddef.h>
+#include "variadic_functions.h"
+
 int main(void)
 {
-list_t *head;
-list_t *new;
-list_t hello = {"World", 5, NULL};
-size_t n;
-head = &hello;
-new = malloc(sizeof(list_t));
-if (new == NULL)
-{
-printf("Error\n");
-return (1);
-}
-new->str = strdup("Hello");
-new->len = 5;
-new->next = head;
-head = new;
-n = list_len(head);
-printf("-> %lu elements\n", n);
-free(new->str);
-free(new);
+print_numbers(", ", 4, 0, 98, -1024, 402);
+print_numbers(NULL, 3, 1, 2, 3);
 return (0);
 }
--- singly_linked_lists/README.md
+++ singly_linked_lists/README.md
@@ -1,8 +1,6 @@
-# Singly Linked Lists
 
-This directory contains tasks related to singly linked lists. The main objective is to understand and implement basic operations like adding nodes and calculating the length of a linked list.
+Task 3: print_all
+Implements print_all(const char * const format, ...).
+Prints anything based on format string: c, i, f, s.
+Handles NULL strings as "(nil)" and separates values with ", ".
 
-## Files
-
-- **1-list_len.c**: A function that returns the number of elements in a linked list.
-- **2-add_node.c**: A function that adds a new node at the beginning of a list.
