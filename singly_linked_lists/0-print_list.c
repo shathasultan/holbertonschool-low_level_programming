9ff812b372ce256f953b9751c3f8840b1318e19f
shathasultan <shathasultann9@gmail.com>
2025-10-18 15:05:48 +0000

    Fix add_node function and related files

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,16 +1,31 @@
 #include "lists.h"
-#include <stdio.h>
+#include <unistd.h>
+#include <stdio.h>  /* Added to support snprintf and strlen */
+#include <string.h>  /* Added to support strlen */
 
+/**
+ * print_list - Prints all the elements of a list_t list.
+ * @h: Pointer to the head of the list.
+ *
+ * Return: The number of nodes.
+ */
 size_t print_list(const list_t *h)
 {
     size_t count = 0;
+    char buffer[100];  /* Buffer to hold the string for write */
 
     while (h)
     {
         if (h->str)
-            printf("[%u] %s\n", h->len, h->str);
+        {
+            /* Print the length of the string and the string itself */
+            snprintf(buffer, sizeof(buffer), "[%u] %s\n", h->len, h->str);
+            write(1, buffer, strlen(buffer));
+        }
         else
-            printf("[0] (nil)\n");
+        {
+            write(1, "[0] (nil)\n", 11);  /* Print if string is NULL */
+        }
         h = h->next;
         count++;
     }
